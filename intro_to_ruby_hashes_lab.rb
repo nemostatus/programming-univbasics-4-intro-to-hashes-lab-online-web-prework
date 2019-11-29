e506d8b99f78c9d6b890270951dac46d6ec4a350
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-11-29 06:08:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,9 +23,7 @@ my_hash_creator("Grace","Hopper")
 
 
 def read_from_hash(hash, key)
-  return hash = {key: "steve", key: "tsvi"}
-end
-read_from_hash("hash","steve")
+  
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
